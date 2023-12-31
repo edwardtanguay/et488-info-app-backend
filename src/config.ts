604b94df8d28ac268a228b4d84f179bef68d79a0
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-31 15:26:33 +0100

    added app name

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,7 @@ export const getPort = (): number => {
 	return 4887;
 }
 
+export const getAppName = (): string => {
+	return 'Info App - Backend'
+}
+
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/', (req, res) => {
 	res.json({
-		appName: "et488-info-app-backend"
+		appName: config.getAppName()
 	});
 });
 
