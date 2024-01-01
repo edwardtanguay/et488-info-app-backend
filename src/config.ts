a101c16855ea0403813e81780b3fb8f439938060
Edward Tanguay (windows) <edward@tanguay.info>
2024-01-01 13:41:31 +0100

    added env variable

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,13 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const getPort = (): number => {
 	return 4887;
 }
 
 export const getAppName = (): string => {
-	return 'Info App 488 - Backend Version'
+	const appName = process.env.APP_NAME;
+	return appName ? appName : 'ERROR: APP_NAME NOT DEFINED';
 }
 
